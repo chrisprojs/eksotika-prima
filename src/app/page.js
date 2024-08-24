f9bfd85f27939da52bf4e3d3bedb0430ef089603
chrisprojs <christiananggaresta20@gmail.com>
2024-08-24 15:13:13 +0700

    Fix: Home Area Text

--- src/app/page.js
+++ src/app/page.js
@@ -7,12 +7,17 @@ export default function Home() {
     <>
       <div className='home-img-container'>
       <Image src="/asset/home_template_1.jpg" alt="home img template" className='home-img-template' width={1024} height={256}></Image>
-        <p className='home-img-text'>Supplier Produk Makassar<br/>
+        <p className='home-img-text'>Supplier Minyak Gosok Terbesar Se-Indonesia<br/>
           <span className='home-img-text2'>Harga Termurah, Bisa Nego</span>
         </p>
         <div className='home-blast-sign'>
           <p>Beli Banyak Nego Banyak!</p>
         </div>
+      </div>
+      <div className='home-area-box'>
+        <p className="home-area-text">
+          Makassar - Ambon
+        </p>
       </div>
       <div className='page-container'>
         <p className='page-heading'>Produk Kami</p>
